d19be05066e1faea62a6161f6d0a95201d25df1a
luzeevaart <luzeevaart@gmail.com>
2023-12-19 14:22:45 -0500

    setting up

---
+++ Advent of Code/AOC_1:1:23.py
@@ -0,0 +1,8 @@
+def read_input(puzzle):
+    with open(f'inputs/{puzzle}.txt', 'r') as f:
+        data = f.read().splitlines()
+    return data
+
+
+git config --global user.email "luzeevaart@gmail.com"
+  git config --global user.name "luzeevaart"
